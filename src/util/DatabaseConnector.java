66ae4cacd2d04d559caff26c81999bf8510ea588
PPIG2204 <phuocnnn2006@gmail.com>
2025-11-10 15:34:46 +0700

    sua ti

--- src/util/DatabaseConnector.java
+++ src/util/DatabaseConnector.java
@@ -6,23 +6,25 @@ import java.sql.SQLException;
 
 public class DatabaseConnector {
     private static final String URL = "jdbc:sqlite:database/quanlydiem.db";
-    private static Connection conn = null;
+    
 
     private DatabaseConnector() {}
 
     public static Connection getConnection() {
-        if (conn == null) {
-            try {
-                Class.forName("org.sqlite.JDBC"); // bắt buộc để load driver
-                conn = DriverManager.getConnection(URL);
-                System.out.println("thanh cong");
-            } catch (ClassNotFoundException e) {
-                System.out.println("khong tim thay driver: " + e.getMessage());
-            } catch (SQLException e) {
-                System.out.println(" loi ket noi: " + e.getMessage());
+    
+        try {
+            Class.forName("org.sqlite.JDBC"); // bắt buộc để load driver
+            Connection conn = DriverManager.getConnection(URL);
+            if (conn != null && !conn.isClosed()){
+                System.out.println("thanh cong");    
             }
+            return conn;
+        } catch (ClassNotFoundException e) {
+            System.out.println("khong tim thay driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println(" loi ket noi: " + e.getMessage());
         }
-        return conn;
+        return null;
     }
 
     public static void main(String[] args) {
